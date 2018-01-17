0236dde057fc83183c22b03d198be868ff8c3fb2
breeze073 <2011805376@qq.com>
2018-01-17 11:15:35 +0800

    简单适配

---
+++ app/src/main/java/com/example/umeyeNewSdk/test.java
@@ -0,0 +1,17 @@
+//package com.example.umeyeNewSdk;
+//
+///**
+// * Created by QING on 2018/1/16.
+// */
+//
+//public class test {
+//
+//    PLAYER_STATE_READY = 0;          // 准备中
+//    PLAYER_STATE_CONNECTING = 1;     // 连接中
+//    PLAYER_STATE_PLAY = 2;           // 播放中
+//    PLAYER_STATE_CONNECT_FAIL = 3;   // 连接失败
+//    PLAYER_STATE_STOP = 4;           // 已停止
+//    PLAYER_STATE_PAUSE = 6;          // 暂停中（回放才有）
+//    PLAYER_STATE_STOPING = 7;        // 正在停止
+//    PLAYER_STATE_LOADING = 10;       // 缓冲中
+//}
